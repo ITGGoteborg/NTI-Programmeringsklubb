da5e8551001d7b08d692bdd00b01291512d4a8ff
Currysos <nohj.gbg@gmail.com>
2019-12-03 20:09:50 +0100

    Main content

--- scripts/MainFunctions.js
+++ scripts/MainFunctions.js
@@ -5,3 +5,9 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0vw";
 }
+
+function TimeToNextMeeting() {
+    var today = new Date();
+    var NextMeeting = new Date();
+    NextMeeting.setDate(NextMeeting.getDate() + (1 + 7 - NextMeeting.getDay()) % 7);
+}
