45f0c298287c37c636fd944dd78125db97f6176b
Currysos <nohj.gbg@gmail.com>
2019-12-09 15:57:24 +0100

    Done!

--- scripts/MainFunctions.js
+++ scripts/MainFunctions.js
@@ -1,5 +1,13 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "61vw";
+    if (window.matchMedia("(orientation: portrait)").matches) {
+        //PORTRAIT
+        document.getElementById("mySidenav").style.width = "61vw";
+    }
+
+    if (window.matchMedia("(orientation: landscape)").matches) {
+        // you're in LANDSCAPE mode
+        document.getElementById("mySidenav").style.width = "29vw";
+    }
 }
 
 function closeNav() {
